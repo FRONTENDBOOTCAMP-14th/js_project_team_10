d3ae69d9c3c921d125789e912c0667f3b37b0037
cit1566 <cit1566@gmail.com>
2025-07-29 14:32:02 +0900

    [feat] page-5 행성 비교 페이지 피드백 수정 - 좌우 행성 이동 스크롤량 조절 -> 스크롤량 늘림 - 주 행성 초점 맞춤 - x축 이동값 조정 - 행성 간 거리 조정 - 태양 애니메이션 추가

--- src/components/page-5-planet-comparison/planet-comparison.js
+++ src/components/page-5-planet-comparison/planet-comparison.js
@@ -6,63 +6,94 @@ let planets = gsap.utils.toArray(".planet__contents .planet");
 let scrollTween = gsap.to(planets, {
   scrollTrigger: {
     trigger: ".page_5-container",
-    start: "300px top",
+    start: "top top",
+    end: () => "+=15000",
     pin: true,
     scrub: true,
-    end: () => "+=3000",
     // markers: true,
   },
-  x: () => -1000,
 });
 
 // 수성 기준 scale 설정
-gsap.set(".mercury", {
-  scale: 1,
-  transformOrigin: "bottom left",
-  rotate: "360deg",
-  repeat: -1,
-});
+// gsap.set(".mercury", { scale: 1, transformOrigin: "bottom left" });
+// gsap.set(".mars", { scale: 1.2, transformOrigin: "bottom left" });
+// gsap.set(".venus", { scale: 2, transformOrigin: "bottom left" });
+// gsap.set(".earth", { scale: 3, transformOrigin: "bottom left" });
+// gsap.set(".neptune", { scale: 12, transformOrigin: "bottom left" });
+// gsap.set(".uranus", { scale: 13, transformOrigin: "bottom left" });
+// gsap.set(".saturn", { scale: 27, transformOrigin: "bottom left" });
+// gsap.set(".jupiter", { scale: 28, transformOrigin: "bottom left" });
+// gsap.set(".sun", { scale: 200, transformOrigin: "bottom left" });
 
-gsap.set(".mars", { scale: 1.2, transformOrigin: "bottom left" });
-gsap.set(".venus", { scale: 2, transformOrigin: "bottom left" });
-gsap.set(".earth", { scale: 3, transformOrigin: "bottom left" });
-gsap.set(".neptune", { scale: 6, transformOrigin: "bottom left" });
+gsap.set(".mercury", { scale: 1, transformOrigin: "bottom left" });
+gsap.set(".mars", { scale: 1.25, transformOrigin: "bottom left" });
+gsap.set(".venus", { scale: 2.25, transformOrigin: "bottom left" });
+gsap.set(".earth", { scale: 2.7, transformOrigin: "bottom left" });
+gsap.set(".neptune", { scale: 9.75, transformOrigin: "bottom left" });
 gsap.set(".uranus", { scale: 10, transformOrigin: "bottom left" });
-gsap.set(".saturn", { scale: 20, transformOrigin: "bottom left" });
+gsap.set(".saturn", { scale: 23.5, transformOrigin: "bottom left" });
 gsap.set(".jupiter", { scale: 28, transformOrigin: "bottom left" });
-gsap.set(".sun", { scale: 200, transformOrigin: "bottom left" });
-gsap.set(".planet__contents", { scale: 1.8 });
-
-gsap.to(".planet img", {
-  ScrollTrigger: {
-    trigger: ".page_5-contents",
-    start: "top center",
-    toggleAction: "play pause play puase",
-    markers: true,
-  },
-  rotation: 360,
-  repeat: -1,
-  duration: 10,
-  ease: "none",
-  transformOrigin: "center center",
+gsap.set(".sun", {
+  scale: 272.5,
+  transformOrigin: "bottom left",
+  y: -5000,
+  x: -175000,
+  opacity: 0,
 });
+gsap.set(".sun p", { opacity: 0 });
+gsap.set(".planet__contents", { scale: 1.8 });
 
 // 스케일 타임라인
 const tl = gsap.timeline();
 
-tl.to(".planet__contents", {
-  opacity: 1,
-  scale: 0.01,
-  ease: "none",
-  duration: 10,
-  ease: "power1.inOut",
-  onEnter: () => console.log("start"),
-});
+// tl.to(".planet__contents", { scale: 1.5, x: -1450, duration: 3 })
+//   .to(".planet__contents", { scale: 0.9, x: -1700, duration: 3 })
+//   .to(".planet__contents", { scale: 0.65, x: -2350, duration: 3 })
+//   .to(".planet__contents", { scale: 0.3, x: -1700, duration: 3 })
+//   .to(".planet__contents", { scale: 0.13, x: -1200, duration: 3 })
+//   .to(".planet__contents", { scale: 0.1, x: -1500, duration: 3 });
+
+tl.to(".planet__contents", { scale: 1.4, x: -1220, duration: 2 })
+  .to(".planet__contents", { scale: 0.8, x: -1560, duration: 2 })
+  .to(".planet__contents", { scale: 0.7, x: -2800, duration: 2 })
+  .to(".planet__contents", { scale: 0.187, x: -1100, duration: 2 })
+  .to(".planet__contents", { scale: 0.118, x: -1550, duration: 2 })
+  .to(".planet__contents", { scale: 0.078, x: -1800, duration: 2 })
+  .to(".planet__contents", { scale: 0.06, x: -2600, duration: 2 })
+  .to(".planet__contents", { scale: 0.0065, x: 1000, duration: 2 })
+  .to(".sun", { y: -20000, duration: 3, opacity: 1, duration: 2 })
+  .to(".sun p", { opacity: 1 })
+  .to(".sun img", { rotation: 360, repeat: 0.1, duration: 5 });
 
 ScrollTrigger.create({
   trigger: ".page_5-container",
   start: "top top",
-  end: "+=3000",
-  scrub: 0.3,
+  end: "+=12000",
+  scrub: 0.5,
   animation: tl,
+});
+
+gsap.to(".mercury img", {
+  scrollTrigger: {
+    trigger: ".mercury",
+    // start: "top top",
+    // end: "center center",
+    toggleAction: "play pause play pause",
+    horizontal: true,
+    containerAnimation: scrollTween,
+  },
+  rotation: 360,
+  repeat: -1,
+  duration: 30,
+  ease: "none",
+  // transformOrigin: "center center",
+});
+
+ScrollTrigger.create({
+  trigger: ".mercury",
+  // start: "top top",
+  // end: "center center",
+  toggleAction: "play pause play pause",
+
+  // markers: true,
 });
