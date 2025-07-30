a511696c459c53f465b26189400ccb0221731b91
cit1566 <cit1566@gmail.com>
2025-07-30 15:34:07 +0900

    [feat] page-5 행성 비교 페이지 모바일 디자인 구성

--- src/components/page-5-planet-comparison/planet-comparison.js
+++ src/components/page-5-planet-comparison/planet-comparison.js
@@ -16,52 +16,153 @@ let scrollTween = gsap.to(planets, {
   },
 });
 
-// 수성 기준 scale 설정
-
-gsap.set(".mercury", { scale: 1, transformOrigin: "bottom left" });
-gsap.set(".mars", { scale: 1.25, transformOrigin: "bottom left" });
-gsap.set(".venus", { scale: 2.25, transformOrigin: "bottom left" });
-gsap.set(".earth", { scale: 2.7, transformOrigin: "bottom left" });
-gsap.set(".neptune", { scale: 9.75, transformOrigin: "bottom left" });
-gsap.set(".uranus", { scale: 10, transformOrigin: "bottom left" });
-gsap.set(".saturn", { scale: 23.5, transformOrigin: "bottom left" });
-gsap.set(".jupiter", { scale: 28, transformOrigin: "bottom left" });
-gsap.set(".sun", {
-  scale: 272.5,
-  transformOrigin: "bottom left",
-  y: -5000,
-  x: -175000,
-  opacity: 0,
+gsap.to(".planet img", {
+  scrollTrigger: {
+    trigger: ".planet",
+    start: "top top",
+    end: "center center",
+    toggleAction: "play pause play pause",
+    horizontal: true,
+    containerAnimation: scrollTween,
+  },
+  rotation: 360,
+  repeat: -1,
+  // duration: 30,
+  ease: "none",
+  // transformOrigin: "center center",
 });
-gsap.set(".sun p", { opacity: 0 });
+
 // gsap.set(".planet__contents", { scale: 1.8 });
 
 // 스케일 타임라인
 // 모바일-----------------------------------------------------------------------
 mm.add("(max-width : 1279px", () => {
+  // 수성 기준 scale 설정
   gsap.set(".planet__contents", { scale: 2 });
   gsap.set(".title", { x: centerPlanet(".title") * 0.5 });
+  gsap.set(".planet", { y: 300 });
+  gsap.set(".mercury", { scale: 1, transformOrigin: "top left" });
+  gsap.set(".mars", { scale: 1.25, transformOrigin: "top left" });
+  gsap.set(".venus", { scale: 2.25, transformOrigin: "top left" });
+  gsap.set(".earth", { scale: 2.7, transformOrigin: "top left" });
+  gsap.set(".neptune", { scale: 9.75, transformOrigin: "top left" });
+  gsap.set(".uranus", { scale: 10, transformOrigin: "top left" });
+  gsap.set(".saturn", { scale: 23.5, transformOrigin: "top left" });
+  gsap.set(".jupiter", { scale: 28, transformOrigin: "top left" });
+  gsap.set(".sun", {
+    scale: 272.5,
+    transformOrigin: "top left",
+    // y: -50000,
+    x: -50000,
+    opacity: 0,
+  });
+  gsap.set(".sun p.sun-name", { opacity: 0 });
+  gsap.set(".sun .description span", { opacity: 0 });
 
   const t2 = gsap.timeline();
 
-  t2.to(".title h1 p:nth-child(1)", { opacity: 1, duration: 0.1 })
-    .to(".title h1 p:nth-child(1)", { opacity: 0, duration: 0.1 })
-    .to(".title h1 p:nth-child(2)", { opacity: 1, duration: 0.1 })
-    .to(".title h1 p:nth-child(2)", { opacity: 0, duration: 0.1 })
-    .to(".title", { x: centerPlanet(".title"), duration: 2 })
-    // .to(".planet__contents", { scale: 5, x: -3000, duration: 2 });
-
-  // ScrollTrigger.create({
-  //   trigger: ".page_5-container",
-  //   start: "top top",
-  //   end: "+=15000",
-  //   scrub: 0.5,
-  //   animation: t2,
-  // });
+  t2.to(".title h1 p:nth-child(1)", { opacity: 1, duration: 1 })
+    .to(".title h1 p:nth-child(1)", { opacity: 0, duration: 1 })
+    .to(".title h1 p:nth-child(2)", { opacity: 1, duration: 1 })
+    .to(".title h1 p:nth-child(2)", { opacity: 0, duration: 1 })
+    // 수성
+    .to(".planet__contents", { x: -185, y: -1000, scale: 3, duration: 3 })
+    .to(".mercury .is-active", { opacity: 1, duration: 2 })
+    // 화성
+    .to(".planet__contents", {
+      x: -125,
+      y: centerPlanetY(".mars") * 0.4,
+      scale: 2.3,
+      duration: 2,
+    })
+    .to(".mars .is-active", { opacity: 1, duration: 2 })
+    // 금성
+    .to(".planet__contents", {
+      x: centerPlanet(".venus") * 0.6,
+      y: centerPlanetY(".venus") * 0.31,
+      scale: 1.6,
+      duration: 2,
+    })
+    .to(".venus .is-active", { opacity: 1, duration: 2 })
+    // 지구
+    .to(".planet__contents", {
+      x: centerPlanet(".earth") * 0.34,
+      y: centerPlanetY(".earth") * 0.27,
+      scale: 1.3,
+      duration: 2,
+    })
+    .to(".earth .is-active", { opacity: 1, duration: 2 })
+    // 해왕성
+    .to(".planet__contents", {
+      x: centerPlanet(".neptune") * 0.01,
+      y: centerPlanetY(".neptune") * 0.05,
+      scale: 0.36,
+      duration: 2,
+    })
+    .to(".neptune .is-active", { opacity: 1, duration: 2 })
+    // 천왕성
+    .to(".planet__contents", {
+      x: 35,
+      y: centerPlanetY(".uranus") * 0.06,
+      scale: 0.28,
+      duration: 2,
+    })
+    .to(".uranus .is-active", { opacity: 1, duration: 2 })
+    // 토성
+    .to(".planet__contents", {
+      x: 0,
+      y: -500,
+      scale: 0.15,
+      duration: 2,
+    })
+    .to(".saturn .is-active", { opacity: 1, duration: 2 })
+    // 목성
+    .to(".planet__contents", {
+      x: 50,
+      scale: 0.1,
+      duration: 2,
+    })
+    .to(".jupiter .is-active", { opacity: 1, duration: 2 })
+    .to(".planet__contents", { x: 50, y: 100, scale: 0.01, duration: 2 })
+    .to(".sun", { x: -102, y: 0, opacity: 1, duration: 2 })
+    .to(".sun .sun-name", { opacity: 1, duration: 2 })
+    .to(".sun .is-active", { opacity: 1, duration: 2 })
+    .to(".sun img", { rotation: 360, repeat: 0.1, duration: 2 })
+    .to(".planet__contents", { scale: 0.1, x: -1050, y: -3100, duration: 5 })
+    .to(".sun .description span:nth-child(1)", { opacity: 1, duration: 2 })
+    .to(".sun .description span:nth-child(1)", { opacity: 0, duration: 2 })
+    .to(".sun .description span:nth-child(2)", { opacity: 1, duration: 2 })
+    .to(".sun .description span:nth-child(2)", { opacity: 0, duration: 2 });
+  ScrollTrigger.create({
+    trigger: ".page_5-container",
+    start: "top top",
+    end: "+=15000",
+    scrub: 0.5,
+    animation: t2,
+  });
 });
 
 // ------------------------------- 데스크탑, 테블릿 --------------------
 mmover.add("(min-width: 1280px)", () => {
+  // 수성 기준 scale 설정
+
+  gsap.set(".mercury", { scale: 1, transformOrigin: "bottom left" });
+  gsap.set(".mars", { scale: 1.25, transformOrigin: "bottom left" });
+  gsap.set(".venus", { scale: 2.25, transformOrigin: "bottom left" });
+  gsap.set(".earth", { scale: 2.7, transformOrigin: "bottom left" });
+  gsap.set(".neptune", { scale: 9.75, transformOrigin: "bottom left" });
+  gsap.set(".uranus", { scale: 10, transformOrigin: "bottom left" });
+  gsap.set(".saturn", { scale: 23.5, transformOrigin: "bottom left" });
+  gsap.set(".jupiter", { scale: 28, transformOrigin: "bottom left" });
+  gsap.set(".sun", {
+    scale: 272.5,
+    transformOrigin: "bottom left",
+    y: -5000,
+    x: -175000,
+    opacity: 0,
+  });
+  gsap.set(".sun p.sun-name", { opacity: 0 });
+  gsap.set(".sun .description span", { opacity: 0 });
   gsap.set(".planet__contents", { scale: 1.8 });
   gsap.set(".title", { x: centerPlanet(".title") * 0.4 });
 
@@ -125,10 +226,18 @@ mmover.add("(min-width: 1280px)", () => {
     .to(".jupiter .is-active", { opacity: 1, duration: 2 })
     .to(".planet__contents", { x: 1000, scale: 0.0065, duration: 2 })
     .to(".sun", { y: -20000, opacity: 1, duration: 2 })
-    .to(".sun p", { opacity: 1, duration: 2 })
+    .to(".sun p.sun-name", { opacity: 1, duration: 2 })
     .to(".sun .is-active", { opacity: 1, duration: 2 })
-    .to(".sun img", { rotation: 360, repeat: 0.1, duration: 2 });
-
+    // .to(".sun img", { rotation: 360, repeat: 0.1, duration: 2 })
+    .to(".planet__contents", {
+      scale: 0.03,
+      x: 1500,
+      y: 2100,
+      duration: 2,
+    })
+    .to(".sun .description span:nth-child(1)", { opacity: 1, duration: 2 })
+    .to(".sun .description span:nth-child(1)", { opacity: 0, duration: 2 })
+    .to(".sun .description span:nth-child(2)", { opacity: 1, duration: 2 });
   ScrollTrigger.create({
     trigger: ".page_5-container",
     start: "top top",
@@ -136,22 +245,6 @@ mmover.add("(min-width: 1280px)", () => {
     scrub: 0.5,
     animation: tl,
   });
-});
-
-gsap.to(".planet img", {
-  scrollTrigger: {
-    trigger: ".planet",
-    start: "top top",
-    end: "center center",
-    toggleAction: "play pause play pause",
-    horizontal: true,
-    containerAnimation: scrollTween,
-  },
-  rotation: 360,
-  repeat: -1,
-  // duration: 30,
-  ease: "none",
-  // transformOrigin: "center center",
 });
 
 // 행성 중심 PX 값
@@ -165,12 +258,12 @@ function centerPlanet(planetSelector) {
   return Math.floor(-offset);
 }
 
-function classAdd(selector, className) {
-  const element = document.querySelector(selector);
-  element.classList.add(className);
-}
+function centerPlanetY(planetSelector) {
+  const planet = document.querySelector(planetSelector);
+  const rect = planet.getBoundingClientRect();
+  const planetCenter = rect.top + rect.bottom / 2;
+  const viewprotCenter = window.innerHeight / 2;
+  const offset = planetCenter - viewprotCenter;
 
-function classRemove(selector, className) {
-  const element = document.querySelector(selector);
-  element.classList.remove(className);
+  return Math.floor(-offset);
 }
