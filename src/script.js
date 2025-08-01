397277747e55063e00ed82d82bca51382216f043
cit1566 <cit1566@gmail.com>
2025-08-01 21:05:18 +0900

    [feat] 팀 소개 페이지 애니메이션 완성

--- src/script.js
+++ src/script.js
@@ -1,10 +1,19 @@
 gsap.registerPlugin(ScrollTrigger);
 const profileTimeLine = gsap.timeline();
 
-gsap.set(".team-section-intro", { scale: 0.8, opacity: 0.3 });
+gsap.set(".team-section-intro", { scale: 0.8 });
+gsap.set(".team-section-wrap", { opacity: 0.2 });
 gsap.set(".text-2, .text-3, .text-4", { opacity: 0 });
 profileTimeLine
-  .to(".team-section-intro", { scale: 1, opacity: 1, duration: 2 })
+  .to(".team-section-intro", {
+    scale: 1.02,
+    opacity: 1,
+    duration: 2,
+  })
+  .to(".team-section-wrap", {
+    opacity: 1,
+    duration: 2,
+  })
   .to(".p-wrap .panel, .text-wrap .panel-text", {
     y: -603,
     duration: 2,
@@ -33,60 +42,91 @@ ScrollTrigger.create({
     const resulte = (a - scaleDownA) / 2;
     return "-=" + resulte;
   },
-  end: "+=3000",
+  end: "+=5000",
   pin: true,
-  scrub: 0.3,
+  scrub: 3,
   animation: profileTimeLine,
   // markers: true,
 });
 
-// text Scroll
-const pTag1 = document.querySelector(".first-parallel");
-const textArr1 = "TenTrillion TenTrillion TenTrillion".split();
-let count1 = 0;
-initTexts(pTag1, textArr1);
+const spacingBox = document.querySelector(".element-spacing");
+spacingBox.style.setProperty("height", () => {
+  const introBoxHight = document.querySelector(
+    ".team-section-intro"
+  ).scrollHeight;
 
-function initTexts(element, textArray) {
-  textArray.push(...textArray);
-  for (let i = 0; i < textArray.length; i++) {
-    element.innerText += `${textArray[i]}\u00A0\u00A0\u00A0\u00A0`;
-  }
-}
-
-function marqueeText(count, element, direction) {
-  if (count > element.scrollWidth / 2) {
-    element.style.transform = `translate3d(0, 0, 0)`;
-    count = 0;
-  }
-  element.style.transform = `translate3d(${direction * count}px, 0, 0)`;
-  return count;
-}
-
-function animate() {
-  count1++;
-  count1 = marqueeText(count1, pTag1, -1);
-  window.requestAnimationFrame(animate);
-}
-
-function scrollHandler() {
-  count1 += 15;
-}
-window.addEventListener("scroll", scrollHandler);
-// animate();
+  console.log(introBoxHight);
+  return introBoxHight + "px";
+});
 
 // ------- project-intro
 const projectIntro = gsap.timeline();
 
-projectIntro
-  .to(".project-intro", { backgroundColor: "black", duration: 0.5 })
-  .to(".project-intro", { backgroundColor: "#a17ed5", duration: 1 });
+gsap.set(".intro-list-1, .intro-list-2, .intro-list-3, .intro-list-4", {
+  opacity: 0,
+  x: 3300,
+  y: -300,
+});
 
-scrollTrigger.create({
+projectIntro
+  .to(".project-intro", { backgroundColor: "black", duration: 1 })
+  .to(".project-intro", { backgroundColor: "#fa8072", duration: 1 })
+  .to(".intro-list-1", { opacity: 1, duration: 1 }, 3)
+  .to(
+    ".project-intro ul",
+    {
+      x: () => {
+        const a =
+          document.querySelector(".project-intro-box").scrollWidth - 1700;
+        console.log(a);
+        return "-" + a;
+      },
+      duration: 30,
+    },
+    3
+  )
+  .to(".project-intro ul", { opacity: 0, duration: 2 })
+  // 동시 효과
+  .to(".intro-list-1", { opacity: 0, duration: 1 }, 8.5)
+  .to(".intro-list-2", { opacity: 1, duration: 1 }, 6)
+  .to(".intro-list-3", { opacity: 1, duration: 1 }, 10)
+  .to(".intro-list-2", { opacity: 0, duration: 1 }, 12)
+  .to(".intro-list-4", { opacity: 1, duration: 1 }, 14)
+  .to(".intro-list-3", { opacity: 0, duration: 1 }, 18.5);
+
+ScrollTrigger.create({
   trigger: ".project-intro",
-  start: "top top",
-  // end: "+=1000",
+  start: () => {
+    return "top top";
+  },
+  end: "+=5000",
   pin: true,
-  scrub: 0.3,
+  scrub: 3,
   animation: projectIntro,
+  markers: true,
+});
+
+gsap.set(".description-box", { opacity: 0 });
+
+const description = gsap.timeline();
+
+description
+  .to(".division-box", { backgroundColor: "#ffffff", duration: 2 }, 0)
+  .to(".division-box h1, p", { color: "#000000" }, 0)
+  .to(".description-box", { opacity: 1, duration: 2 }, 0)
+  .to(".division-box p", { y: 200, duration: 2 }, 0)
+  .to(".user", { borderRadius: 300, duration: 2 })
+  .to(".kdk", { x: -750, duration: 4 }, 5)
+  .to(".name1", { x: 750, duration: 4 }, 5)
+  .to(".name2", { x: -250, duration: 4 }, 5)
+  .to(".name3", { x: 250, duration: 4 }, 5);
+
+ScrollTrigger.create({
+  trigger: ".division-box",
+  start: "+20 top",
+  end: "+=5000",
+  pin: true,
+  scrub: 3,
+  animation: description,
   markers: true,
 });
